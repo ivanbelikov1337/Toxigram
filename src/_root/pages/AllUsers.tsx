ae3972733fad05a9583b7792b7afcfea5e4517eb
ivanbelikov1337 <120462639+ivanbelikov1337@users.noreply.github.com>
2023-12-24 03:31:20 +0200

    fix AllUsers.tsx

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -2,11 +2,14 @@ import {useToast} from "../../components/ui/use-toast.ts";
 import {useGetUsers} from "../../lib/reactQuery/queriesAndMutations.ts";
 import Loader from "../../components/shared/Loader.tsx";
 import UserCard from "../../components/shared/UserCard.tsx";
+import {useUserContext} from "../../context/AuthContext.tsx";
 
 const AllUsers = () => {
     const { toast } = useToast();
 
     const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
+    const {user: currentUser} = useUserContext();
+    const fixArrayCreators = creators?.documents.filter((arg) => arg.$id !== currentUser.id)!
 
     if (isErrorCreators) {
         toast({ title: "Something went wrong." });
@@ -19,13 +22,13 @@ const AllUsers = () => {
         <div className="common-container">
             <div className="user-container">
                 <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
-                {isLoading && !creators ? (
+                {isLoading && !fixArrayCreators ? (
                     <Loader />
                 ) : (
                     <ul className="user-grid">
-                        {creators?.documents.map((creator) => (
+                        {fixArrayCreators.map((creator) => (
                             <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
-                                <UserCard user={creator} />
+                                <UserCard user={creator}  currentUser={currentUser.id}/>
                             </li>
                         ))}
                     </ul>
